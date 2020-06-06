4e6ba0f40c9c386f0b9bd123bc1577e50ab1eb4a
Glitch (fishy-cyclic-philosophy) <none>
2020-06-06 16:29:31 +0000

    Bai6

--- server.js
+++ server.js
@@ -3,49 +3,49 @@
 
 // we've started you off with Express (https://expressjs.com/)
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-app.set('view engine', 'pug');
-app.set('views', './views');
-app.use(bodyParser.json()) // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var low = require("lowdb");
+var FileSync = require("lowdb/adapters/FileSync");
+
+var adapter = new FileSync("db.json");
+var db = low(adapter);
+app.set("view engine", "pug");
+app.set("views", "./views");
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 // https://expressjs.com/en/starter/basic-routing.html
-var data = [
-  {id: 1, job: 'Di cho'},
-  {id: 2, job: 'Nau com'},
-  {id: 3, job: 'rua bat'},
-  {id: 4, job: 'Di choi'}
-]
-app.get('/', (request, response) => {
-  response.render('index');
+db.defaults({ todolist: [] }).write();
+app.get("/", (request, response) => {
+  response.render("index");
 });
-app.post('/', function (req, res) {
-  res.send('POST request to homepage')
-})
-app.get('/users', (req, res) => {
-  res.render('users/index', {
-    users: data
+// app.post("/", function(req, res) {
+//   res.send("POST request to homepage");
+// });
+app.get("/users", (req, res) => {
+  res.render("users/index", {
+    users: db.get('todolist').value()
   });
 });
-app.get('/users/search',(req, res) => {
+app.get("/users/search", (req, res) => {
   var q = req.query.q;
-  var matchUsers = data.filter((x) => {
+  var matchUsers = db.get('todolist').value().filter(x => {
     return x.job.toLowerCase().indexOf(q.toLowerCase()) !== -1;
   });
-  res.render('users/index', {
+  res.render("users/index", {
     users: matchUsers
   });
-})
-app.get('/users/create', (req, res) => {
-  res.render('users/create', {
-    users: data
+});
+app.get("/users/create", (req, res) => {
+  res.render("users/create", {
+    users: db.get("todolist").value()
   });
-})
-app.post('/users/create', (req, res) => {
-  data.push(req.body);
-  res.redirect('/users');
-})
+});
+app.post("/users/create", (req, res) => {
+  db.get("todolist").push(req.body).write();
+  res.redirect("/users");
+});
 // listen for requests :)<
 app.listen(process.env.PORT, () => {
   console.log("Server listening on port " + process.env.PORT);
