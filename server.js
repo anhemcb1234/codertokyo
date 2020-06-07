5ae6be97a0dc79dcf97e34897ce60fe71554d59b
Glitch (fishy-cyclic-philosophy) <none>
2020-06-07 09:20:15 +0000

    Bai7.1

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var app = express();
 var bodyParser = require("body-parser");
 var low = require("lowdb");
 var FileSync = require("lowdb/adapters/FileSync");
+const shortid = require('shortid');
 
 var adapter = new FileSync("db.json");
 var db = low(adapter);
@@ -19,31 +20,58 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 db.defaults({ todolist: [] }).write();
 app.get("/", (request, response) => {
   response.render("index");
+  //console.log(db.get('todolist').value()[0].id);
 });
-// app.post("/", function(req, res) {
-//   res.send("POST request to homepage");
-// });
 app.get("/users", (req, res) => {
   res.render("users/index", {
-    users: db.get('todolist').value()
+    users: db.get("todolist").value()
   });
 });
 app.get("/users/search", (req, res) => {
   var q = req.query.q;
-  var matchUsers = db.get('todolist').value().filter(x => {
-    return x.job.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-  });
+  var matchUsers = db
+    .get("todolist")
+    .value()
+    .filter(x => {
+      return x.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+    });
   res.render("users/index", {
     users: matchUsers
   });
+});
+app.get("/users/delete", (req, res) => {
+  res.render("users/delete", {
+    users: db.get("todolist").value()
+  });
+});
+app.get("/users/delete/:id", (req, res) => {
+  var id = req.params.id;
+  for(var i = 0; i < db.get('todolist').value().length; i++){;
+    if(id === db.get('todolist').value()[i].id) {
+      db.get('todolist').value().splice(i,1);
+    }
+  }
+  res.redirect("/users");
 });
 app.get("/users/create", (req, res) => {
   res.render("users/create", {
     users: db.get("todolist").value()
   });
+  res.redirect("/users");
 });
+app.get("/users/:id", (req, res) => {
+  var id = req.params.id;
+  var user = db.get('todolist').find({id:id}).value();
+  res.render('users/view', {
+    user: user
+  }) 
+})
+
 app.post("/users/create", (req, res) => {
-  db.get("todolist").push(req.body).write();
+  req.body.id = shortid.generate();
+  db.get("todolist")
+    .push(req.body)
+    .write();
   res.redirect("/users");
 });
 // listen for requests :)<
